9dca3a1d39eafd411c24611876cd626ea64e9a36
M Banks <montensia1@outlook.com>
2019-05-21 18:41:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
-  BRANDS
+  BRANDS = []
   
   def initialize(brand)
     @brand = brand
@@ -13,4 +13,8 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  def brand=(brand)
+    @brand = brand
+    BRANDS << @brand
+  end
 end
